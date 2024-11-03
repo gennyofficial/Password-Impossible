f66ba5e29bcf19b3e7a8adb373d13abfff98b0c0
officialgenny <officialgennychristo@gmail.com>
2024-11-03 07:37:18 +0530

    second

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/Password-Impossible/" : "/",
   chainWebpack: config => {
     config.resolve.set('fallback', {
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
